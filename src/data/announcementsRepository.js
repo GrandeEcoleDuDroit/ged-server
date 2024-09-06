53e4e76ecb469b26b2594984d964a18f4b82301c
Mourchidi Mfoumby <mfoumbymourchidi@gmail.com>
2024-09-06 17:18:58 +0000

    correct get all announcement query

--- src/data/announcementsRepository.js
+++ src/data/announcementsRepository.js
@@ -19,14 +19,9 @@ class AnnouncementsRepository {
         }
 
         const query = `
-            SELECT JSON_OBJECT(
-                'ANNOUNCEMENT_ID' VALUE TO_NUMBER(ANNOUNCEMENT_ID),
-                'ANNOUNCEMENT_TITLE' VALUE TO_CHAR(ANNOUNCEMENT_TITLE),
-                'ANNOUNCEMENT_CONTENT' VALUE TO_CHAR(ANNOUNCEMENT_CONTENT),
-                'ANNOUNCEMENT_DATE' VALUE TO_NUMBER(ANNOUNCEMENT_DATE),
-                'USER_ID' VALUE TO_NUMBER(USER_ID)
-            )
-            FROM ANNOUNCEMENTS;
+            SELECT JSON_OBJECT(*)
+            FROM ANNOUNCEMENTS
+            NATURAL JOIN USERS
         `;
 
         const resultRequest = await this.#oracleConnection.execute(query);
