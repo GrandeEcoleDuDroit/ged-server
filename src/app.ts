ff6d45d516f242d5aa6a2f6acd8cb21927be4ee9
Mourchidi <mourchidi.mfoumby@universite-paris-saclay.fr>
2026-01-13 22:00:23 +0100

    use root path from environment file in app.js

--- src/app.ts
+++ src/app.ts
@@ -8,22 +8,16 @@ import routes from '@routes/index';
 import {setupExpressErrorHandler} from '@sentry/node';
 import { d } from '@utils/logs';
 import OracleApi from '@api/oracleApi';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import { PORT, NODE_ENV, SSL_KEY_PATH, SSL_CERT_PATH } from './env'
+import { ROOT_PATH, PORT, NODE_ENV, SSL_KEY_PATH, SSL_CERT_PATH } from './env'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const ROOT = path.join(__dirname, '../');
 const oracleApi = OracleApi.instance;
 const app = express();
 const productionEnvironment = NODE_ENV == 'production';
 
-app.use(express.static(path.join(ROOT, 'public')));
+app.use(express.static(path.join(ROOT_PATH, 'public')));
 app.get('/', (_: Request, res: Response) => {
-    res.sendFile(path.join(ROOT, 'public/index.html'));
+    res.sendFile(path.join(ROOT_PATH, 'public/index.html'));
 });
-app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
